96ad19be186f076e595c3e3a5314afa81db354de
Siddharth Venkatesan <neotamizhan@gmail.com>
2020-06-25 22:25:07 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayboardComponent } from './playboard/playboard.component';
+import { OriginalsPanelComponent } from './originals-panel/originals-panel.component';
+import { LatestEpisodeComponent } from './latest-episode/latest-episode.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayboardComponent
+    PlayboardComponent,
+    OriginalsPanelComponent,
+    LatestEpisodeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/playboard/playboard.component.ts
+++ src/app/playboard/playboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {QfrItem} from '../qfrItem';
+import { QfrService } from '../qfr.service';
 
 @Component({
   selector: 'app-playboard',
@@ -8,20 +9,17 @@ import {QfrItem} from '../qfrItem';
 })
 export class PlayboardComponent implements OnInit {
 
-  item: QfrItem = {
-    episode_num: 1,
-    date: new Date('2020-03-26'),
-    movie: 'KOVIL PURA',
-    song: 'Sangeethame',
-    year: '1981',
-    original_artists: ['Ilayaraja', 'S.Janaki', 'Vaali'],
-    qfr_artists: ['Saindhavi Prakash'],
-    episode_url: 'https://www.youtube.com/watch?v=UJqEL3OLQMo',
-    song_url: 'https://www.youtube.com/watch?v=UJqEL3OLQMo'
-  };
-  constructor() { }
+  episodes: QfrItem[];
+
+  constructor(private qfrService: QfrService) { }
+
+  getEpisodes():void {
+    this.qfrService.getAllEpisodes()
+    .subscribe(e => this.episodes = e);
+  }
 
   ngOnInit() {
+    this.getEpisodes();
   }
 
 }
