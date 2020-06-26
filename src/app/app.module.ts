b027f76c7e822b2ff6fdf119604dfe2e9972f2eb
Siddharth <siddhu.v@gmail.com>
2020-06-26 18:41:18 +0300

    Revert "added components"
    This reverts commit 96ad19be186f076e595c3e3a5314afa81db354de.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayboardComponent } from './playboard/playboard.component';
-import { OriginalsPanelComponent } from './originals-panel/originals-panel.component';
-import { LatestEpisodeComponent } from './latest-episode/latest-episode.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayboardComponent,
-    OriginalsPanelComponent,
-    LatestEpisodeComponent
+    PlayboardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/playboard/playboard.component.ts
+++ src/app/playboard/playboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< HEAD
 import { QfrItem } from '../qfrItem';
 import { QfrService } from '../qfr.service';
+=======
+import {QfrItem} from '../qfrItem';
+>>>>>>> parent of 96ad19b... added components
 
 @Component({
   selector: 'app-playboard',
@@ -9,6 +13,7 @@ import { QfrService } from '../qfr.service';
 })
 export class PlayboardComponent implements OnInit {
 
+<<<<<<< HEAD
   episodes: QfrItem[];
 
   constructor(private qfrService: QfrService) { }
@@ -21,6 +26,22 @@ export class PlayboardComponent implements OnInit {
   ngOnInit() {
     this.getEpisodes();
     console.log(this.episodes);
+=======
+  item: QfrItem = {
+    episode_num: 1,
+    date: new Date('2020-03-26'),
+    movie: 'KOVIL PURA',
+    song: 'Sangeethame',
+    year: '1981',
+    original_artists: ['Ilayaraja', 'S.Janaki', 'Vaali'],
+    qfr_artists: ['Saindhavi Prakash'],
+    episode_url: 'https://www.youtube.com/watch?v=UJqEL3OLQMo',
+    song_url: 'https://www.youtube.com/watch?v=UJqEL3OLQMo'
+  };
+  constructor() { }
+
+  ngOnInit() {
+>>>>>>> parent of 96ad19b... added components
   }
 
 }
